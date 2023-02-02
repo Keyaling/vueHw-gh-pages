210274525b57a8127753481e8f29a982e507f403
Keyaling <linda811008@gmail.com>
2023-02-02 23:36:16 +0800

    '更新資料'

---
+++ hwWeek04/Pagination.js
@@ -0,0 +1,23 @@
+//在template加入{{page}}可直接確認外層資料是否有傳入內層;:key="page + 'page'"成立唯一值，避免重複；如果page === pages.current_page，就會套上active這個class
+export default {
+    props:['pages','getProductData'],
+    template:`
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+        <li class="page-item" :class="{ disabled:!pages.has_pre }">
+            <a class="page-link" href="#" aria-label="Previous" @click="getProductData(pages.current_page - 1)">
+            <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+        <li class="page-item" 
+        :class="{ active: page === pages.current_page}" v-for="page in pages.total_pages" :key="page + 'page'">
+            <a class="page-link" href="#" @click.prevent="getProductData(page)">{{page}}</a>
+        </li>
+        <li class="page-item" :class="{ disabled:!pages.has_next }">
+            <a class="page-link" href="#" aria-label="Next" @click="getProductData(pages.current_page + 1)">
+            <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
+        </ul>
+    </nav>`
+};
